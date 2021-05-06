7fd43c564657d5a2f336fa9494fc727dfc6cca0a
supAmorn <supanuthaa@gmail.com>
2021-05-07 00:47:04 +0700

    Update today script
    Add docker proxy and using ask-become-pass in the /sh script
    but add_host still bug

--- Ansible script/run-nectar.sh
+++ Ansible script/run-nectar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-. ./both-openrc.sh; ansible-playbook -i hosts docker_demo.yaml
+. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts.ini --ask-become-pass create_instance.yaml
+. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts.ini --ask-become-pass setup_env.yaml
