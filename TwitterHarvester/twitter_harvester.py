70f4a70786ce86682df6eaa97f32326064c3d31c
pprh1 <82828866+pprh1@users.noreply.github.com>
2021-05-06 18:47:46 +1000

    Update twitter_harvester.py

--- TwitterHarvester/twitter_harvester.py
+++ TwitterHarvester/twitter_harvester.py
@@ -36,8 +36,28 @@ class StdOutListener(StreamListener):
           decoded = json.loads(data)
           with open('harvested_tweets.json','a') as tf:
             json_load = json.loads(data)
-            #text = {'text': json_load['text']}
-            tf.write(json.dumps(json_load))
+            user = {'id_str': json_load['user']['id_str'],
+                    'screen_name': json_load['user']['screen_name'],
+                    'location': json_load['user']['location'],
+                    'verified': json_load['user']['verified'],
+                    'followers_count': json_load['user']['followers_count'],
+                    'friends_count': json_load['user']['friends_count'],}
+            text = {'created_at': json_load['created_at'],
+                    'id_str': json_load['id_str'],
+                     'text': json_load['text'],
+                    'source': json_load['source'],
+                    'user': user,
+                    'geo': json_load['geo'],
+                    'coordinates': json_load['coordinates'],
+                    'place': json_load['place'],
+                    'quote_count': json_load['quote_count'],
+                    'reply_count': json_load['reply_count'],
+                    'retweet_count': json_load['retweet_count'],
+                    'favorite_count': json_load['favorite_count'],
+                    'entities': json_load['entities'],
+                    'lang': json_load['lang'],
+                    }
+            tf.write(json.dumps(text))
     
     def on_error(self,status):
         print(status)
