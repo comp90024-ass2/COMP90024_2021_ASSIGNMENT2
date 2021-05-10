1c9c457f5522161cff216d3aad90132e69ab29cd
supAmorn <supanuthaa@gmail.com>
2021-05-11 00:09:06 +0700

    Update Merge ansible script

--- Ansible script/run-nectar.sh
+++ Ansible script/run-nectar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts.ini --ask-become-pass create_instance.yaml
-. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts.ini --ask-become-pass setup_env.yaml
+#. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts.ini --ask-become-pass create_instance.yaml
+. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts --ask-become-pass main_instance.yaml
+#. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts.ini --ask-become-pass setup_env.yaml
+. ./unimelb-comp90024-2021-grp-36-openrc.sh; ansible-playbook -i hosts --ask-become-pass couchdb-setup.yaml
