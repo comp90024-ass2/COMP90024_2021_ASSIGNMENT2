f1f1e37c634b5d583d58a0f0b4c7a8d672e23417
pprh1 <82828866+pprh1@users.noreply.github.com>
2021-05-13 05:41:01 +1000

    Update tweepy_code.py
    1. Minor fix to make sure the latest tweet id get updated to the next iteration
    2. Add a condition to exit the script if there is no more tweet from the specified date
    3. Broaden the scope of harvesting by looking at 14 most recent tweets from user timeline for every tweet from the query result.

--- TwitterHarvester/tweepy_code.py
+++ TwitterHarvester/tweepy_code.py
@@ -17,6 +17,9 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
+#any today's tweet (for example the most recent Scott Morrison tweet)
+current_tweet_id = '1392339238188818437'
+
 #couchdb connect
 couch = couchdb.Server(url=tw_cdb_credentials.url)
 couch.resource.credentials =tw_cdb_credentials.login
@@ -27,6 +30,9 @@ def get_tweets_query(qword,geocodes,page,date, current_id):
     # Authorization to consumer key and consumer secret
     # Calling api
     api = tweepy.API(auth)
+    last_tweet_id = current_id
+    search_tweets_inserted = 0
+    timeline_tweets_inserted = 0
   
     pages = tweepy.Cursor(api.search, q=qword, geocode=geocodes, count=100, max_id = current_id, until=date).pages(page)
     #pages = tweepy.Cursor(api.search, q=qword, geocode=geocodes, count=100, until=date).pages(page)
@@ -42,7 +48,7 @@ def get_tweets_query(qword,geocodes,page,date, current_id):
                     'friends_count': json_load['user']['friends_count'],}
             text = {'_id': json_load['id_str'],
                     'created_at': json_load['created_at'],
-                     'text': json_load['text'],
+                    'text': json_load['text'],
                     'source': json_load['source'],
                     'user': user,
                     'geo': json_load['geo'],
@@ -55,15 +61,53 @@ def get_tweets_query(qword,geocodes,page,date, current_id):
                     'entities': json_load['entities'],
                     'lang': json_load['lang'],
                     }
+            last_tweet_id = tweet.id_str
+            #anticipate duplicate tweets from the search result
             try:
                 db.save(json.loads(json.dumps(text)))
-            #continue iteration and ignore duplicate tweet (not saved in db)
+                search_tweets_inserted = search_tweets_inserted + 1
+                print("save search tweet successful ", last_tweet_id)
+                #go through user timeline of the tweet from search query result
+                tweets_user = api.user_timeline(id=json_load['user']['id_str'],count=15,max_id=json_load['id_str'])
+                for tweet_user in tweets_user:
+                    tweetstr = json.dumps(tweet_user._json)
+                    json_load = json.loads(tweetstr)
+                    user = {'id_str': json_load['user']['id_str'],
+                            'screen_name': json_load['user']['screen_name'],
+                            'location': json_load['user']['location'],
+                            'verified': json_load['user']['verified'],
+                            'followers_count': json_load['user']['followers_count'],
+                            'friends_count': json_load['user']['friends_count'],}
+                    text = {'_id': json_load['id_str'],
+                            'created_at': json_load['created_at'],
+                            'text': json_load['text'],
+                            'source': json_load['source'],
+                            'user': user,
+                            'geo': json_load['geo'],
+                            'coordinates': json_load['coordinates'],
+                            'place': json_load['place'],
+                            'retweet_count': json_load['retweet_count'],
+                            'favorite_count': json_load['favorite_count'],
+                            'entities': json_load['entities'],
+                            'lang': json_load['lang'],
+                            }
+                    #anticipate duplicate tweets from the user timeline
+                    try:
+                        db.save(json.loads(json.dumps(text)))
+                        print("save user timeline tweet")
+                        timeline_tweets_inserted = timeline_tweets_inserted + 1
+                    except couchdb.http.ResourceConflict:
+                        print("duplicate user timeline tweet")
+
             except couchdb.http.ResourceConflict:
-                print("duplicate tweet")
-            current_id = tweet.id_str
+                print("duplicate search tweet")
+                print(last_tweet_id)
+                #sys.exit(1)
+            #current_tweet_id = tweet.id_str
         print('searching paused')
-        print('current_id' + current_id)
-    
+        print("Search tweets:", search_tweets_inserted, " timeline tweets:", timeline_tweets_inserted)
+    return (last_tweet_id,search_tweets_inserted)
+
 if __name__ == "__main__":
     
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -73,32 +117,35 @@ if __name__ == "__main__":
     #geocodes = "-37.8136,144.9631,10mi"
 
     #Sydney
-    geocodes = "-33.8136,151,10mi"
+    #geocodes = "-33.8136,151,10mi"
 
     #Brisbane
     #geocodes = "-27.48,153,10mi"
 
     #Perth
-    #geocodes = "-31.96,115.85,10mi"
+    geocodes = "-31.96,115.85,10mi"
 
     #Adelaide
     #geocodes = "-34.917,138.6,10mi"
 
     qword = ""
     page = 100 # maximum pages we can get within 15min
-    date = "2021-05-03"
-    current_id = '1388805726622175232' # updated 7:13 pm 10/05/2021
+    date = "2021-05-06"
+    #current_tweet_id = '1389369176683278337' # updated 7:13 pm 10/05/2021 
     timer = 900
     
     # run for 999 times just in case you forgot to close it
     for i in range(0, 999):
         timer = 900
-        couch = couchdb.Server(url=tw_cdb_credentials.url)
-        couch.resource.credentials =tw_cdb_credentials.login
-        db = couch[tw_cdb_credentials.dbname]
-        print('time to abstract from ' + date + '. With tweets earlier than: ' + current_id)
-        get_tweets_query(qword,geocodes,page,date, current_id)
         
+        print('time to abstract from ' + date + '. With tweets earlier than: ' + current_tweet_id)
+        result = get_tweets_query(qword,geocodes,page,date, current_tweet_id)
+        current_tweet_id = result[0]
+        print(current_tweet_id)
+
+        if(result[1]==0):
+            sys.exit()
+                    
         # make call after 15min
         while timer >= 0:
             time.sleep(1)
