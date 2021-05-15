af127b3c36f3307eb52c236e0559de3b25e679d0
supAmorn <supanuthaa@gmail.com>
2021-05-15 17:22:24 +0700

    Update Twitter_harvester code

--- Backend/tw_cdb_credentials.py
+++ Backend/tw_cdb_credentials.py
@@ -6,7 +6,7 @@ url = "http://172.26.131.86:5984/"
 login = ('admin', 'admin')
 
 #uncomment the database of the city that is gonna be searched
-dbname = 'twitter_melbourne'
+#dbname = 'twitter_melbourne'
 #dbname = 'twitter_sydney'
 #dbname = 'twitter_brisbane'
 #dbname = 'twitter_perth'
