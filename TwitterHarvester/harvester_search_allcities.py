22b32d485b884f4c65dfdd116882738dbfeea155
pprh1 <82828866+pprh1@users.noreply.github.com>
2021-05-21 13:35:09 +1000

    Update harvester_search_allcities.py
    The script now will change city after 100 pages of a query result

--- TwitterHarvester/harvester_search_allcities.py
+++ TwitterHarvester/harvester_search_allcities.py
@@ -7,6 +7,7 @@ import logging
 import datetime as DT
 import tw_cdb_credentials
 import sys
+from http.client import IncompleteRead
 
 #twitter auth
 consumer_key = tw_cdb_credentials.consumer_key
@@ -14,13 +15,13 @@ consumer_secret = tw_cdb_credentials.consumer_secret
 access_token = tw_cdb_credentials.access_token
 access_token_secret = tw_cdb_credentials.access_token_secret
 
-#1:perth, 2:sydney, 3:melbourne, 4:brisbane, 5:adelaide
+#1:melbourne, 2:brisbane, 3:sydney, 4:adelaide, 5:perth
 citycode = 1
 
 # == couchdb ==
 couch = couchdb.Server(url=tw_cdb_credentials.url)
 couch.resource.credentials = tw_cdb_credentials.login
-db = couch['twitter_perth']
+db = couch['twitter_melbourne']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -91,7 +92,7 @@ def get_tweets_query(qword,geocodes,page,datetweet, current_id):
                 search_tweets_inserted = search_tweets_inserted + 1
                 logging.info("save search tweet successful " + last_tweet_id)
                 #go through user timeline of the tweet from search query result
-                tweets_user = api.user_timeline(id=json_load['user']['id_str'],count=5,max_id=json_load['id_str'])
+                tweets_user = api.user_timeline(id=json_load['user']['id_str'],count=2,max_id=json_load['id_str'])
                 for tweet_user in tweets_user:
                     tweetstr = json.dumps(tweet_user._json)
                     json_load = json.loads(tweetstr)
@@ -125,7 +126,6 @@ def get_tweets_query(qword,geocodes,page,datetweet, current_id):
                 logging.info(last_tweet_id)
         logging.info('Page:' + str(page) + " Tweet number:" + str(tweetnumber))
     currenttime = time.time()
-    logging.info('searching paused 15 minutes')
     logging.info("Search tweets:" + str(search_tweets_inserted) + " timeline tweets:" + str(timeline_tweets_inserted) + " Runtime of the program is: " + str(DT.timedelta(seconds=currenttime-starttime)))
     return (last_tweet_id,search_tweets_inserted)
 
@@ -136,12 +136,12 @@ if __name__ == "__main__":
 
     starttime = time.time()
 
-    #Adelaide
-    geocodes = "-31.96,115.85,10mi"
-    logging.info("SYDNEY")
+    #Melbourne
+    geocodes = "-27.48,153,10mi"
+    logging.info("MELBOURNE")
 
     qword = ""
-    page = 99 # maximum pages we can get within 15min
+    page = 50 # half maximum pages we can get within 15min
     datetweet = (DT.date.today() - DT.timedelta(days=6)).strftime("%Y-%m-%d")
     timer = 900
     
@@ -149,49 +149,107 @@ if __name__ == "__main__":
     for i in range(0, 999):
         cityname = ""
 
-        if citycode == 5:
-            cityname = "Adelaide"
-        elif citycode == 1:
+        if citycode % 5 == 0:
             cityname = "Perth"
-        elif citycode == 2:
+        elif citycode % 5 == 4:
+            cityname = "Adelaide"
+        elif citycode % 5 == 3:
             cityname = "Sydney"
-        elif citycode == 3:
+        elif citycode % 5 == 1:
             cityname = "Melbourne"
-        elif citycode == 4:
+        elif citycode % 5 == 2:
             cityname = "Brisbane"
         
+        zeroresultcountm = 0
+        zeroresultcountb = 0
+        zeroresultcounts = 0
+        zeroresultcounta = 0
+        zeroresultcountp = 0
+
+        lasttweetidm = ""
+        lasttweetidb = ""
+        lasttweetids = ""
+        lasttweetida = ""
+        lasttweetidp = ""
+
         logging.info("query number:" + str(i) + ' time to abstract ' + cityname + ' tweets from ' + datetweet + '. With tweets earlier than: ' + current_tweet_id)
         result = get_tweets_query(qword,geocodes,page,datetweet, current_tweet_id)
         current_tweet_id = result[0]
         logging.info("Tweet #" + current_tweet_id + " Tweets inserted:" + str(result[1]))
-
-        if(result[1]==0):
-            citycode = citycode + 1
-            if citycode == 2:
-                db = couch['twitter_sydney']
-                geocodes = "-33.8136,151,10mi"
+        
+        citycode = citycode + 1
+        if citycode % 5 == 0:
+            db = couch['twitter_perth']
+            geocodes = "-31.96,115.85,10mi"
+            logging.info("PERTH")
+            lasttweetidp = current_tweet_id
+            if citycode < 6:
                 current_tweet_id = begin_tweet_id
-                logging.info("SYDNEY")
-            elif citycode == 3:
-                db = couch['twitter_melbourne']
-                geocodes = "-37.8136,144.9631,10mi"
-                current_tweet_id = begin_tweet_id
-                logging.info("MELBOURNE")
-            elif citycode == 4:
-                db = couch['twitter_brisbane']
-                geocodes = "-27.48,153,10mi"
-                current_tweet_id = begin_tweet_id
-                logging.info("BRISBANE")
-            elif citycode == 5:
-                db = couch['twitter_adelaide']
-                geocodes = "-34.917,138.6,10mi"
-                current_tweet_id = begin_tweet_id
-                logging.info("ADELAIDE")
             else:
-                sys.exit()
+                current_tweet_id = lasttweetidm
+            if(result[1]==0):
+                zeroresultcounta = zeroresultcounta + 1
+                logging.info("Zero search result Adelaide")
+        elif citycode % 5 == 2:
+            db = couch['twitter_brisbane']
+            geocodes = "-27.48,153,10mi"
+            logging.info("BRISBANE")
+            lasttweetidb = current_tweet_id
+            if citycode < 6:
+                current_tweet_id = begin_tweet_id
+            else:
+                current_tweet_id = lasttweetids
+            if(result[1]==0):
+                zeroresultcountm = zeroresultcountm + 1
+                logging.info("Zero search result Melbourne")
+        elif citycode % 5 == 3:
+            db = couch['twitter_sydney']
+            geocodes = "-33.8136,151,10mi"
+            logging.info("SYDNEY")
+            lasttweetids = current_tweet_id
+            if citycode < 6:
+                current_tweet_id = begin_tweet_id
+            else:
+                current_tweet_id = lasttweetida
+            if(result[1]==0):
+                zeroresultcountb = zeroresultcountb + 1
+                logging.info("Zero search result Brisbane")
+        elif citycode % 5 == 4:
+            db = couch['twitter_adelaide']
+            geocodes = "-34.917,138.6,10mi"
+            logging.info("ADELAIDE")
+            lasttweetida = current_tweet_id
+            if citycode < 6:
+                current_tweet_id = begin_tweet_id
+            else:
+                current_tweet_id = lasttweetidp
+            if(result[1]==0):
+                zeroresultcounts = zeroresultcounts + 1
+                logging.info("Zero search result Sydney")
+        elif citycode % 5 == 1:
+            db = couch['twitter_melbourne']
+            geocodes = "-37.8136,144.9631,10mi"
+            logging.info("MELBOURNE")
+            lasttweetidm = current_tweet_id
+            if citycode < 6:
+                current_tweet_id = begin_tweet_id
+            else:
+                current_tweet_id = lasttweetidb
+            if(result[1]==0):
+                zeroresultcountp = zeroresultcountp + 1
+                logging.info("Zero search result Perth")
 
-        # make call after 15min
-        while timer >= 0:
-            time.sleep(1)
-            timer -= 1
+
+        if(zeroresultcountm > 0 and zeroresultcountb > 0 and zeroresultcounts > 0 and zeroresultcounta > 0 and zeroresultcountp > 0):
+            sys.exit()
+
+        if citycode % 2 == 1:
+            logging.info('searching paused 15 minutes')
+            # make call after 15min
+            while timer >= 0:
+                time.sleep(1)
+                timer -= 1
+        else:
+            logging.info('No pause')
+
     logging.info('searching finished')
