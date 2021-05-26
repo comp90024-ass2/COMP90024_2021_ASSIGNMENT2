8a4886e912ca5bb2417a477af1aeafdf3882f2ce
patawat watakul <patawat.wtk@gmail.com>
2021-05-26 10:42:03 +1000

    update ip of frontend

--- Frontend/assignment2-map/src/utils/request.js
+++ Frontend/assignment2-map/src/utils/request.js
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = false;
 
 
 const request = axios.create({
-  baseURL: "http://172.26.131.86:5984",
+  baseURL: "http://172.26.131.157:5984",
   timeout: 10000,
   withCredentials: false,
   headers: {
