4a08b7d0552e078e1387e3ced9477bab4cda015a
supAmorn <supanuthaa@gmail.com>
2021-05-26 07:19:43 +0700

    Update final url for twitter_credential

--- Ansible script/roles/harvester-docker/templates/tw_cdb_credentials.py
+++ Ansible script/roles/harvester-docker/templates/tw_cdb_credentials.py
@@ -2,7 +2,7 @@ consumer_key = "NoryoSniMdErXzEVDhJwbzqB4"
 consumer_secret = "Ody8CGKGK8veyfQ4fxrfY4wpBDQeb9uJDW7EDOcjkMxLt8KV0c"
 access_token = "1383699993186373632-wQhjCdBWZCVytHZGLQL4A06LRNXhBx"
 access_token_secret = "FZRjxSuc82tWyFdlROshZiktt67qFtdzcCmaQDFoVcPa4"
-url = "http://172.26.131.86:5984/"
+url = "http://172.26.131.157:5984/"
 login = ('admin', 'admin')
 
 #uncomment the database of the city that is gonna be searched
